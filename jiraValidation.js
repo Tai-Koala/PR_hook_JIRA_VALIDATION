3f13353a00289800a21bca9e6f99c707e9b9314e
Tai LE <tle@neteck-fr.com>
2021-01-19 14:19:39 +0100

    #2

--- jiraValidation.js
+++ jiraValidation.js
@@ -1,3 +1,32 @@
-const ticketId = process.env['TICKET_ID'];
+const axios =  require('axios');
+const dotenv = require('dotenv');
 
-console.log(ticketId);
+dotenv.config();
+
+const ticketId = process.env['TICKET_ID'];
+const url = `${process.env['API_BASE_URL']}/issue/${ticketId}`;
+const auth = {
+    username: process.env['JIRA_USERNAME'],
+    password: process.env['JIRA_API_KEY']
+};
+
+(async () => {
+    console.log(`Checked ticket ${ticketId}`);
+
+    try {
+        const {data} = await axios.get(url, {auth});
+
+        if (data.fields.issuelinks) {
+            const isBlockedAndNoDone = data.fields.issuelinks.filter((issue) => !((issue.type.inward === 'is blocked by') && ('inwardIssue' in issue) && (issue.inwardIssue.fields.status.statusCategory.id === 3)));
+
+            if (isBlockedAndNoDone.length === 0) {
+                process.exitCode = 0;
+            } else {
+                process.exitCode = 1;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        process.exitCode = 1;
+    }
+})();
